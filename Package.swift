36f283c2e446a254f89a7413321f22aabdb8c3d8
Jonny <jonny.mortensen@daon.com>
2024-07-02 13:23:29 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,17 +9,17 @@ let package = Package(
             targets: ["DaonCryptoSDK"]),
     ],
     targets: [
-//        .binaryTarget(
-//            name: "DaonCryptoSDK",
-//            path: "Frameworks/DaonCryptoSDK.xcframework")
-
-        .binaryTarget(
+        .package(
             name: "DaonCryptoSDK",
-            url: "https://nexus.identityx-build.com/repository/sdk-packages/crypto/DaonCryptoSDK-1.0.6.xcframework.zip",
-            checksum: "118806c52e5750f07cdbe35da3f5412c577135b1eb9518774edb791ad8c3a016"
-            // url: "https://github.com/jonnymortensen/sdk-crypto/releases/download/1.0.5/DaonCryptoSDK.xcframework.zip",
-            // checksum: "ffc30d691cff68c5de803b6ff66c6d0bd57daf129abb025f45aaaa7bfbc64bbc"
-        )
+            path: "DaonCryptoSDK-1.0.6.xcframework"
+        ),
+        // .binaryTarget(
+        //     name: "DaonCryptoSDK",
+        //     url: "https://nexus.identityx-build.com/repository/sdk-packages/crypto/DaonCryptoSDK-1.0.6.xcframework.zip",
+        //     checksum: "118806c52e5750f07cdbe35da3f5412c577135b1eb9518774edb791ad8c3a016"
+        //     // url: "https://github.com/jonnymortensen/sdk-crypto/releases/download/1.0.5/DaonCryptoSDK.xcframework.zip",
+        //     // checksum: "ffc30d691cff68c5de803b6ff66c6d0bd57daf129abb025f45aaaa7bfbc64bbc"
+        // )
         ]
 )
 //let package = Package(
