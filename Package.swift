19c356801e03d4097296b105a4200f11335f7e5f
Jonny <jonny.mortensen@daon.com>
2024-07-02 14:20:37 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,17 @@
-// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
     name: "DaonCryptoPackage",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+    ],
     products: [
         .library(
             name: "DaonCryptoPackage",
-            targets: ["DaonCryptoPackage", "DaonCryptoSDK"]),
+            targets: ["DaonCryptoPackage", "DaonCryptoSDK"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
         .target(
@@ -16,12 +21,5 @@ let package = Package(
             name: "DaonCryptoSDK",
             path: "DaonCryptoSDK-1.0.6.xcframework"
         )
-        // .binaryTarget(
-        //     name: "DaonCryptoSDK",
-        //     url: "https://nexus.identityx-build.com/repository/sdk-packages/crypto/DaonCryptoSDK-1.0.6.xcframework.zip",
-        //     checksum: "118806c52e5750f07cdbe35da3f5412c577135b1eb9518774edb791ad8c3a016"
-        //     // url: "https://github.com/jonnymortensen/sdk-crypto/releases/download/1.0.5/DaonCryptoSDK.xcframework.zip",
-        //     // checksum: "ffc30d691cff68c5de803b6ff66c6d0bd57daf129abb025f45aaaa7bfbc64bbc"
-        // )
-        ]
+    ]
 )
